cdc6ae93fb10dac5fa997bb4e92988499d753131
Martín Rosso <mrosso10@gmail.com>
2025-05-20 14:55:52 -0300

    refactor: move fixture seeds to dummy

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,3 @@
 module Nexo
-  ActiveRecord::FixtureSet.create_fixtures(["#{::Rails.root}/../fixtures"], ["events", "users", "nexo/clients" , "nexo/integrations" , "nexo/tokens" , "nexo/folders"])
-
-  Nexo::Token.first.update(environment: "development")
+  # Nexo seeds
 end
