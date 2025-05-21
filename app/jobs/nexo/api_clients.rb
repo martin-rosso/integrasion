330c060f65ab6d676cc456465a66a3bbd80c3df7
Martín Rosso <mrosso10@gmail.com>
2025-05-21 18:10:58 -0300

    fix: configure job retry wait algorithm

--- app/jobs/nexo/api_clients.rb
+++ app/jobs/nexo/api_clients.rb
@@ -7,6 +7,8 @@ module Nexo
 
       queue_as :api_clients
 
+      # TODO!: configure good job queues and thread pools
+      # https://github.com/bensheldon/good_job?tab=readme-ov-file#optimize-queues-threads-and-processes
       # TODO: make this configurable, so other job backends are allowed
       good_job_control_concurrency_with(
         perform_limit: 1,
@@ -19,7 +21,10 @@ module Nexo
       retry_on(
         GoodJob::ActiveJobExtensions::Concurrency::ConcurrencyExceededError,
         attempts: Float::INFINITY,
-        wait: ->(executions) { ((executions**3) + (Kernel.rand * (executions**3) * 0.5)) + 2 }
+        wait: :polynomially_longer,
+        jitter: 0.99,
+        # wait: ->(executions) { ((executions**4) + (Kernel.rand * (executions**4) * jitter)) + 2 }
+        # wait: ->(executions) { ((executions**3) + (Kernel.rand * (executions**3) * 0.5)) + 2 }
       )
     end
   end
