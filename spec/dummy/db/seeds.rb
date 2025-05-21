d0c9e971c1d4113327accf545f1443dc8f0c3857
Martín Rosso <mrosso10@gmail.com>
2025-05-21 15:46:53 -0300

    test: seeds

--- spec/dummy/db/seeds.rb
+++ spec/dummy/db/seeds.rb
@@ -1,5 +1,4 @@
-ActiveRecord::FixtureSet.create_fixtures(["#{::Rails.root}/../fixtures"], ["events", "users", "nexo/clients" , "nexo/integrations" , "nexo/tokens" , "nexo/folders"])
+# ActiveRecord::FixtureSet.create_fixtures(["#{::Rails.root}/../fixtures"], ["events", "users", "nexo/clients" , "nexo/integrations" , "nexo/tokens" , "nexo/folders"])
 
-Nexo::Token.first.update(environment: "development")
-
+ActiveRecord::FixtureSet.create_fixtures(["#{::Rails.root}/../fixtures"], ["events", "users"])
 Nexo::Engine.load_seed
