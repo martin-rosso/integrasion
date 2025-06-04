69e0d6a51d53c4351280b67cc0158651574a974f
Martín Rosso <mrosso10@gmail.com>
2025-06-04 18:29:16 -0300

    feat: elements controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,18 @@
 Nexo::Engine.routes.draw do
+  resources :elements do
+    member do
+      post :fetch_remote
+      post :resolve_conflict
+      post :modify_local
+      post :update_status
+    end
+  end
+
+  resources :element_versions do
+    member do
+      post :sync
+    end
+  end
+
+  root to: redirect('elements')
 end
