ea4d9d70cc9bc93c2b8e4408bcf3c62e9db1dae5
Martín Rosso <mrosso10@gmail.com>
2025-06-08 11:59:37 -0300

    test: google event payload jsons

--- spec/factories/nexo/element_versions.rb
+++ spec/factories/nexo/element_versions.rb
@@ -12,12 +12,49 @@
 #  updated_at :datetime         not null
 #  nev_status :integer          not null
 #
+def read_payload_json(name)
+  JSON.parse(File.read(Nexo::Engine.root.join("spec/factories/nexo/element_version_payloads", "#{name}.json")))
+end
+
 FactoryBot.define do
   factory :nexo_element_version, class: "Nexo::ElementVersion" do
     association :element, factory: :nexo_element
-    origin { "internal" }
-    payload { "bla" }
-    etag { "asd" }
-    add_attribute(:sequence) { rand(0..10) }
+    origin { %i[internal external].sample }
+    payload { read_payload_json("google_calendar_event_with_time") }
+
+    trait :with_time do
+      payload { read_payload_json("google_calendar_event_with_time") }
+    end
+
+    trait :all_day do
+      payload { read_payload_json("google_calendar_event_all_day") }
+    end
+
+    trait :synced do
+      etag { "asd" }
+      nev_status { :synced }
+      add_attribute(:sequence) { rand(0..10) }
+    end
+
+    trait :unsynced_local_change do
+      nev_status { :pending_sync }
+      origin { "internal" }
+      add_attribute(:sequence) { rand(0..10) }
+      etag { nil }
+    end
+
+    trait :element_previously_synced do
+      element { create :nexo_element, uuid: Digest::MD5.hexdigest(Faker::Lorem.word) }
+    end
+
+    trait :unsynced_external_change do
+      add_attribute(:sequence) { nil }
+      nev_status { :pending_sync }
+      origin { "external" }
+    end
+
+    after(:create) do |element_version, context|
+      element_version.element.update_ne_status!
+    end
   end
 end
