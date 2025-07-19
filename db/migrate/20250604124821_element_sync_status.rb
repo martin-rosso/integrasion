676f3778cc3bae79acf849758de24e4d5566cfce
Martín Rosso <mrosso10@gmail.com>
2025-07-19 14:57:14 -0300

    fix: nev_status default value

--- db/migrate/20250604124821_element_sync_status.rb
+++ db/migrate/20250604124821_element_sync_status.rb
@@ -1,9 +1,11 @@
 class ElementSyncStatus < ActiveRecord::Migration[7.2]
   def change
-    add_column :nexo_elements, :ne_status, :integer, null: false
+    add_column :nexo_elements, :ne_status, :integer
+    change_column_null :nexo_elements, :ne_status, false, -1
     remove_column :nexo_elements, :conflicted, :boolean, default: false, null: false
 
-    add_column :nexo_element_versions, :nev_status, :integer, null: false
+    add_column :nexo_element_versions, :nev_status, :integer
+    change_column_null :nexo_element_versions, :nev_status, false, -1
 
     add_index :nexo_element_versions, [:element_id, :sequence], unique: true, where: "sequence IS NOT NULL"
     add_index :nexo_element_versions, [:element_id, :etag], unique: true, where: "etag IS NOT NULL"
