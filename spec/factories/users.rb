bd27d7dc854019bd8716425cea516e0064bf667d
Martín Rosso <mrosso10@gmail.com>
2025-07-23 13:28:34 -0300

    test: suppress DuplicateDefinitionError

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,11 @@
-FactoryBot.define do
-  factory :user do
-    email { Faker::Internet.email }
-    password { Faker::Lorem.word }
+begin
+  FactoryBot.define do
+    factory :user do
+      email { Faker::Internet.email }
+      password { Faker::Lorem.word }
+    end
   end
+rescue FactoryBot::DuplicateDefinitionError => e
+  # it may be defined by the main app, anyway this is not bundled with the gem
+  puts e.inspect
 end
